cabab53f96a5aabe833cc304970ed863497da614
KovalYevhenii <kovalyevhenii@gmail.com>
2023-10-22 17:37:45 +0200

    minor changes

--- Library/controller/MenuController.cs
+++ Library/controller/MenuController.cs
@@ -1,60 +1,57 @@
 ﻿using ConDicLibrary.View;
 using static ConDicLibrary.View.Menu;
-
-namespace ConDicLibrary.controller
+namespace ConDicLibrary.controller;
+internal class MenuController
 {
-    internal class MenuController
+    private readonly Menu _menu;
+    private readonly Library _library;
+    public MenuController(Library library)
     {
-        private readonly Menu _menu;
-        private readonly Library _library;
-        public MenuController(Library library)
+        _library = library;
+        Menu menu = new();
+        _menu = menu;
+    }
+    
+    public void Start()
+    {
+        while (true)
         {
-            _library = library;
-            Menu menu = new();
-            _menu = menu;
-        }
-        
-        public void Start()
-        {
-            while (true)
-            {
-                _menu.StartMenu();
-                var input = Console.ReadLine();
+            _menu.StartMenu();
+            var input = Console.ReadLine();
 
-                if (int.TryParse(input, out int res))
+            if (int.TryParse(input, out int res))
+            {
+                switch (res)
                 {
-                    switch (res)
-                    {
-                        case 1:
-                            {
-                                _menu.SetMenuState(MenuState.AddBook);
-                                var bookName = _menu.First();
-                                _library.AddBook(bookName);
-                                break;
-                            }
-                        case 2:
-                            {
-                                _menu.SetMenuState(MenuState.UnreadedBook);
-                                _menu.Second();
-                                _library.ShowAllBooks();
-                                break;
-                            }
-                        case 3:
-                            {
-                                _menu.SetMenuState(MenuState.Exit);
-                                _menu.Close();
-                                Environment.Exit(0);
-                                break;
-                            }
-                        default:
-                            _menu.StartMenu();
+                    case 1:
+                        {
+                            _menu.SetMenuState(MenuState.AddBook);
+                            var bookName = _menu.First();
+                            _library.AddBook(bookName);
                             break;
-                    }
+                        }
+                    case 2:
+                        {
+                            _menu.SetMenuState(MenuState.UnreadedBook);
+                            _menu.Second();
+                            _library.ShowAllBooks();
+                            break;
+                        }
+                    case 3:
+                        {
+                            _menu.SetMenuState(MenuState.Exit);
+                            _menu.Close();
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        _menu.StartMenu();
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("Incorrect input");
-                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
             }
         }
     }
